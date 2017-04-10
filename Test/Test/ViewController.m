55019c70ea552140a95556157c1556125b13cc2e
pthk <pthk@pthkdeMac-mini.local>
2017-04-10 11:14:32 +0800

    Develop提交

--- Test/Test/ViewController.m
+++ Test/Test/ViewController.m
@@ -23,11 +23,41 @@
     btn.backgroundColor = [UIColor orangeColor];
     [self.view addSubview:btn];
     
+    btn.tag = 10;
+    btn.selected = NO;
+    
     [btn setTitle:@"这是一个按钮" forState:UIControlStateNormal];
     [btn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     
+    [btn addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
+    
 }
 
+- (void)btnClick:(UIButton *)Btn
+{
+    static int clickcounts = 0;
+    if (Btn.selected == NO){
+        
+        NSLog(@"%d",clickcounts);
+        [Btn setTitle:[NSString stringWithFormat:@"%ld",Btn.tag] forState:UIControlStateNormal];
+        clickcounts += 1;
+        Btn.selected = !Btn.selected;
+        
+        
+    }else{
+        
+        NSLog(@"%d",clickcounts);
+        [Btn setTitle:@"这是一个按钮" forState:UIControlStateNormal];
+        clickcounts += 1;
+        Btn.selected = !Btn.selected;
+
+    }
+
+    
+    
+    
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
