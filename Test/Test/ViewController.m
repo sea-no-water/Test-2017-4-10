ffa0eda0cbc952e1855e91674ea8deabb383d903
sea-no-water <979729620@qq.com>
2017-04-18 14:50:07 +0800

    Update ViewController.m

--- Test/Test/ViewController.m
+++ Test/Test/ViewController.m
@@ -48,7 +48,7 @@
     DLZHtf.placeholder = @"请输入账号";
     DLZHtf.alpha = 0.5;
     DLZHtf.keyboardType = UIKeyboardTypeNumbersAndPunctuation;
-    //设置返回键为“join”
+    //设置返回键为join 系统键盘
     DLZHtf.returnKeyType = UIReturnKeyJoin;
     
     //密码
